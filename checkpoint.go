8dc4caa9dc2ac38ba624a11072ad06e3c60140be
Renning Bruns <ren@renmail.net>
2023-03-02 19:53:04 -0800

    checkpoint in its own file

---
+++ checkpoint.go
@@ -0,0 +1,50 @@
+package multitailer
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+)
+
+type Checkpoint struct {
+	Offset int64
+	File   *os.File
+	Size   int64
+	Dev    int32
+	Ino    uint64
+}
+
+// Check checks if the file has changed, and re-opens it if it has.
+func (c *Checkpoint) Check(fileName string) error {
+	fileInfo, err := os.Stat(fileName)
+	if err != nil {
+		if os.IsNotExist(err) || os.IsPermission(err) {
+			// file removed or can't read it anymore
+			_ = c.File.Close()
+			return nil
+		}
+		return fmt.Errorf("stat failed: %w", err)
+	}
+	stat, ok := fileInfo.Sys().(*syscall.Stat_t)
+	if !ok {
+		return fmt.Errorf("not a syscall.Stat_t: %v", fileInfo.Sys())
+	}
+
+	// if checkpoint is new (all values 0), or file was moved or truncated, re-open it
+
+	if c.Size > fileInfo.Size() || c.Dev != stat.Dev || c.Ino != stat.Ino {
+		if c.File != nil {
+			_ = c.File.Close()
+		}
+		file, err := os.Open(fileName)
+		if err != nil {
+			return fmt.Errorf("open failed: %w", err)
+		}
+		c.File = file
+		c.Offset = 0
+		c.Dev = stat.Dev
+		c.Ino = stat.Ino
+	}
+	c.Size = fileInfo.Size()
+	return nil
+}
--- checkpoint_reader.go
+++ checkpoint_reader.go
@@ -3,9 +3,7 @@ package multitailer
 import (
 	"fmt"
 	"io"
-	"os"
 	"sync"
-	"syscall"
 )
 
 // CheckpointReader reads lines from files, starting from the last read position.
@@ -66,48 +64,5 @@ func (r *CheckpointReader) Close() error {
 			checkpoint.File = nil
 		}
 	}
-	return nil
-}
-
-type Checkpoint struct {
-	Offset int64
-	File   *os.File
-	Size   int64
-	Dev    int32
-	Ino    uint64
-}
-
-// Check checks if the file has changed, and re-opens it if it has.
-func (c *Checkpoint) Check(fileName string) error {
-	fileInfo, err := os.Stat(fileName)
-	if err != nil {
-		if os.IsNotExist(err) || os.IsPermission(err) {
-			// file removed or can't read it anymore
-			_ = c.File.Close()
-			return nil
-		}
-		return fmt.Errorf("stat failed: %w", err)
-	}
-	stat, ok := fileInfo.Sys().(*syscall.Stat_t)
-	if !ok {
-		return fmt.Errorf("not a syscall.Stat_t: %v", fileInfo.Sys())
-	}
-
-	// if checkpoint is new (all values 0), or file was moved or truncated, re-open it
-
-	if c.Size > fileInfo.Size() || c.Dev != stat.Dev || c.Ino != stat.Ino {
-		if c.File != nil {
-			_ = c.File.Close()
-		}
-		file, err := os.Open(fileName)
-		if err != nil {
-			return fmt.Errorf("open failed: %w", err)
-		}
-		c.File = file
-		c.Offset = 0
-		c.Dev = stat.Dev
-		c.Ino = stat.Ino
-	}
-	c.Size = fileInfo.Size()
 	return nil
 }
